c5e878f88f6acd2ce84de0144ceed31323b870de
Michal Svačina <msvacina@outlook.com>
2025-01-25 11:35:08 +0100

    feat: added femme logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use tide::Request;
+use tide::{Request};
 
 #[tokio::main]
 async fn main() -> tide::Result<()> {
+    if cfg!(debug_assertions) {
+        femme::with_level(femme::LevelFilter::Debug);
+        println!("Debug logging enabled");
+    } else {
+        femme::with_level(femme::LevelFilter::Info);
+        println!("Info logging enabled");
+    }
     let mut app = tide::new();
     app.at("/").get(hello);
     app.listen("0.0.0.0:8080").await?;
@@ -9,6 +16,5 @@ async fn main() -> tide::Result<()> {
 }
 
 async fn hello(mut _req: Request<()>) -> tide::Result<String> {
-    println!("hello returned");
     Ok("hello world".into())
 }
