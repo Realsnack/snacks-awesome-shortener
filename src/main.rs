8960ef70d92ea1122607cc37415ae3d862bb1072
Michal Svačina <msvacina@outlook.com>
2025-02-03 18:34:28 +0100

    feat: add environement variable for used port

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod models;
 mod routes;
 mod services;
 
+use std::ffi::OsString;
+
 use routes::root_routes::init_root_routes;
 
 use tide::log::{debug, info};
@@ -16,10 +18,29 @@ async fn main() -> tide::Result<()> {
         femme::with_level(femme::LevelFilter::Info);
         info!("Info logging enabled");
     }
+
+    let app_address = match std::env::var("SAS_IP") {
+        Ok(address) => address,
+        Err(_) => {
+            info!("SAS_IP not specified, using 0.0.0.0");
+            String::from("0.0.0.0")
+        }
+    };
+
+    let app_port = match std::env::var("SAS_PORT") {
+        Ok(port) => port,
+        Err(_) => {
+            info!("SAS_PORT not specified, using port 8080");
+            String::from("8080")
+        }
+    };
+
     let mut app = tide::new();
 
     init_root_routes(&mut app);
 
-    app.listen("0.0.0.0:8080").await?;
+    let app_listen = format!("{}:{}", app_address, app_port);
+
+    app.listen(app_listen).await?;
     Ok(())
 }
