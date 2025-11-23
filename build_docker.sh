12247357fad445e1f6fb75203dfae48bb7fd33b4
Michal Svačina <msvacina@outlook.com>
2025-11-23 11:22:24 +0100

    chore: updated build_docker.sh to tag image with a version

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-VERSION=$(cat version.txt)
+VERSION=$(grep -Eo '^version = ".*"' Cargo.toml | cut -d\" -f2)
+
+if [ -z "$VERSION" ]; then
+  echo "No version found, exiting..."
+  exit 1
+fi
+
+echo "Running docker build with tags sas:$VERSION and sas:latest"
 
 docker build -t sas:"$VERSION" -t sas:latest ./
