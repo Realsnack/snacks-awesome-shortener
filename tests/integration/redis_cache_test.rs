ad11f372eb7260eb0d77bb06458d5d2e1a2c7d1a
Michal Svačina <msvacina@outlook.com>
2025-12-03 20:13:40 +0100

    test: redis matching short_url test

--- tests/integration/redis_cache_test.rs
+++ tests/integration/redis_cache_test.rs
@@ -1,4 +1,5 @@
 use redis::{AsyncCommands, RedisResult};
+use snacks_awesome_shortener::models::short_url::ShortUrl;
 use crate::integration::common::build_test_env;
 
 #[tokio::test]
@@ -25,6 +26,33 @@ async fn post_short_redis_key_created() {
     let result: RedisResult<String> = connection.get(short_url).await;
 
     assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn post_short_redis_key_created_and_matching() {
+    let test_env = build_test_env(true, true).await;
+    let redis_client = redis::Client::open(test_env.redis_url).unwrap();
+    let mut connection = match redis_client.get_multiplexed_async_connection().await {
+        Ok(connection) => connection,
+        Err(e) => {
+            panic!("Redis connection error: {}", e);
+        }
+    };
+
+    let resp = reqwest::Client::new()
+        .post(format!("http://127.0.0.1:{}/short", test_env.app_port))
+        .json(&serde_json::json!({ "url": "https://hltv.org" }))
+        .send()
+        .await
+        .unwrap();
+
+    let body: serde_json::Value = resp.json().await.unwrap();
+    let short_url = body.get("short_url").unwrap().as_str().unwrap();
+
+    let result: String = connection.get(short_url).await.unwrap();
+    let redis_key: ShortUrl = serde_json::from_str(result.as_str()).unwrap();
+
+    assert_eq!(redis_key.short_url, short_url);
 }
 
 #[tokio::test]
