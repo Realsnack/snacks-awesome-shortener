32f6c7ea476a194e87bb101f17ee039c4ef408dd
Michal Svačina <msvacina@outlook.com>
2025-12-05 20:44:13 +0100

    test: Tests when redis is down

--- tests/integration/api_routes_tests.rs
+++ tests/integration/api_routes_tests.rs
@@ -1,4 +1,4 @@
-use reqwest::redirect;
+use reqwest::{redirect, Error, Response};
 use crate::integration::common;
 use common::build_test_env;
 
@@ -119,6 +119,77 @@ async fn get_short_url_redirect() {
 #[tokio::test]
 async fn get_short_url_object() {
     let test_env = build_test_env(true, true).await;
+    let post_url = "https://hltv.org";
+
+    let resp = reqwest::Client::new()
+        .post(format!("http://127.0.0.1:{}/short", test_env.app_port))
+        .json(&serde_json::json!({ "url": post_url }))
+        .send()
+        .await
+        .unwrap();
+
+    let body: serde_json::Value = resp.json().await.unwrap();
+    let short_url = body.get("short_url").unwrap().as_str().unwrap();
+
+    let url = format!("http://127.0.0.1:{}/short/{}", test_env.app_port, short_url);
+
+    let client = reqwest::Client::builder()
+        .redirect(redirect::Policy::none())
+        .build()
+        .unwrap();
+
+    let resp = client.get(&url).send().await.unwrap();
+    let status = resp.status();
+    let body: serde_json::Value = resp.json().await.unwrap();
+    let long_url = body.get("long_url");
+
+    assert_eq!(status, 200);
+    assert!(long_url.is_some());
+    assert_eq!(long_url.unwrap(), post_url);
+}
+
+#[tokio::test]
+async fn post_short_url_creates_entry_unavailable_redis() {
+    let test_env = build_test_env(false, true).await;
+    let post_url = "https://hltv.org";
+
+    let url = format!("http://127.0.0.1:{}/short", test_env.app_port);
+
+    let resp = reqwest::Client::new()
+        .post(&url)
+        .json(&serde_json::json!({ "url": post_url }))
+        .send()
+        .await;
+
+    let resp = match resp {
+        Ok(resp) => resp,
+        Err(e) => {
+            panic!("Failed to get response from API: {}", e);
+        }
+    };
+
+    let status = resp.status();
+
+    assert_eq!(status, 200);
+
+    println!("resp: {:?}", resp);
+    let body: serde_json::Value = resp.json().await.unwrap();
+    println!("body: {}", body);
+    let short_url = body.get("short_url");
+    let long_url = body.get("long_url");
+    let expiration = body.get("expiration");
+
+    assert_eq!(status, 200);
+    assert!(short_url.is_some());
+    assert_eq!(short_url.unwrap().to_string().len(), 8);
+    assert!(long_url.is_some());
+    assert_eq!(long_url.unwrap(), post_url);
+    assert!(expiration.is_some());
+}
+
+#[tokio::test]
+async fn get_short_url_object_unavailable_redis() {
+    let test_env = build_test_env(false, true).await;
     let post_url = "https://hltv.org";
 
     let resp = reqwest::Client::new()
