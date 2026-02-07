bd8269ec11d1377d28824a0023c9d0e3849db38c
Michal Svačina <msvacina@outlook.com>
2026-02-07 19:22:50 +0100

    feat: Created common config

---
+++ common/src/config.rs
@@ -0,0 +1,59 @@
+use tracing::warn;
+
+#[derive(Debug)]
+pub struct Config {
+    pub request_stream: String,
+    pub response_stream: String,
+    pub consumer_name: String,
+    pub nats_url: String,
+    pub request_stream_max_messages: i64,
+}
+
+impl Config {
+    pub fn new(request_stream: String,
+               response_stream: String,
+               consumer_name: String,
+               nats_url: String,
+               request_stream_max_messages: i64) -> Config {
+        Config {
+            request_stream,
+            response_stream,
+            consumer_name,
+            nats_url,
+            request_stream_max_messages,
+        }
+    }
+
+    pub fn from_env() -> Config {
+        let request_stream = std::env::var("REQUEST_STREAM").unwrap_or_else(|_| {
+            panic!("No REQUEST_STREAM configured");
+        });
+
+        let response_stream = std::env::var("RESPONSE_STREAM").unwrap_or_else(|_| {
+            panic!("No RESPONSE_STREAM configured");
+        });
+
+        let consumer_name = std::env::var("CONSUMER_NAME").unwrap_or_else(|_| {
+            env!("CARGO_PKG_NAME").to_string()
+        });
+
+        let nats_url = std::env::var("NATS_URL").unwrap_or_else(|_| {
+            panic!("No NATS_URL configured");
+        });
+
+        let max_messages = std::env::var("REQUEST_MAX_MESSAGES").unwrap_or_else(|_| {
+            warn!("No REQUEST_MAX_MESSAGES configured - using 1000 as default");
+            String::from("1000")
+        });
+
+        let request_stream_max_messages = max_messages.parse().expect("Couldn't convert REQUEST_MAX_MESSAGES");
+
+        Config {
+            request_stream,
+            response_stream,
+            consumer_name,
+            nats_url,
+            request_stream_max_messages,
+        }
+    }
+}
