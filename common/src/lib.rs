8ecb54e6044bb10f5220305eb980d3cedac10798
Michal Svačina <msvacina@outlook.com>
2026-02-07 19:23:05 +0100

    feat: Created commong logging

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,16 @@
 pub mod models;
+pub mod config;
+pub mod nats_utils;
+
+pub fn setup_logging() {
+    if cfg!(debug_assertions) {
+        tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .init();
+    } else {
+        tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .json()
+            .init();
+    }
+}
