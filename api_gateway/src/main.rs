72ae344c8fe01e9e2f77859970cd6d6b98e81de3
Michal Svačina <msvacina@outlook.com>
2026-02-10 19:40:31 +0100

    Revert "feat: Added instance id into response consumer on api_gateway"
    This reverts commit 58c20353411bd0597b4dafe6e3b594a9d78ec9ae.

--- api_gateway/src/main.rs
+++ api_gateway/src/main.rs
@@ -1,18 +1,12 @@
-use tracing::info;
 use api_gateway::config::Config;
 use api_gateway::{build_app, run, run_consumer};
 use common::setup_logging;
 use common::messaging_config::MessagingConfig;
-use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
     setup_logging();
-    let instance_uuid = Uuid::new_v4().to_string();
-    let instance_id = instance_uuid.split("-").last().unwrap();
-    info!("Instance id: {:?}", instance_id);
-    let mut consumer_config = MessagingConfig::from_env(env!("CARGO_PKG_NAME").to_string());
-    consumer_config.request_stream = format!("{}::{}", consumer_config.response_stream, instance_id);
+    let consumer_config = MessagingConfig::from_env(env!("CARGO_PKG_NAME").to_string());
     let consumer_task = tokio::spawn(run_consumer(consumer_config));
     let api_config = Config::from_env();
     let app = build_app(&api_config).await;
