a3e485b334a5d0fa7c016dd5f3514a4cb9b8d72d
Michal Svačina <msvacina@outlook.com>
2026-04-06 18:45:56 +0200

    feat: Added hostname to api_gateway root endpoint

--- api_gateway/src/handlers/root_handler.rs
+++ api_gateway/src/handlers/root_handler.rs
@@ -1,9 +1,12 @@
 pub async fn handle_root_get() -> String {
     const VERSION: &str = env!("CARGO_PKG_VERSION");
     const NAME: &str = env!("CARGO_PKG_NAME");
+    let hostname = std::env::var("HOSTNAME").unwrap();
+
     format!(
-        r#"{{"application_name": "{}","version": "{}"}}"#,
+        r#"{{"application_name": "{}","version": "{}","hostname":"{}"}}"#,
         NAME,
-        VERSION
+        VERSION,
+        hostname
     )
 }
