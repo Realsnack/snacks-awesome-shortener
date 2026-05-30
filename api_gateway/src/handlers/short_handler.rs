de8bb525562af475623fb35ffb2a590ca4a8cd48
Michal Svačina <msvacina@outlook.com>
2026-05-30 19:32:02 +0200

    feat: add short redirect handler

--- api_gateway/src/handlers/short_handler.rs
+++ api_gateway/src/handlers/short_handler.rs
@@ -3,7 +3,7 @@ use axum::Json;
 use axum::body::Body;
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
+use axum::response::{IntoResponse, Redirect, Response};
 use common::TypeString;
 use common::models::messaging::{
     CreateShortCommand, RetrieveShortCommand, ShortCreatedEvent, ShortRetrievedEvent,
@@ -133,6 +133,65 @@ pub async fn handle_short_get(
                 .status(StatusCode::OK)
                 .body(Body::from(json!(retrieved_short_event.short).to_string()))
                 .unwrap()
+        }
+        _ => {
+            warn!("Timed out waiting for response of '{}'", correlation_id);
+            StatusCode::REQUEST_TIMEOUT.into_response()
+        }
+    }
+}
+
+pub async fn handle_short_redirect(
+    State(state): State<AppState>,
+    headers: axum::http::header::HeaderMap,
+    Path(short_url): Path<String>,
+) -> Response {
+    let correlation_id = get_correlation_id_or_generate(headers);
+
+    let (tx, rx) = oneshot::channel();
+    state.pending.insert(correlation_id.clone(), tx);
+
+    let retrieve_short_command = RetrieveShortCommand::new(short_url);
+    let mut nats_headers = create_common_headers(
+        retrieve_short_command.type_as_string(),
+        correlation_id.clone(),
+    );
+    nats_headers.insert("response_subject", "api_gateway::response");
+
+    match state
+        .client
+        .publish_with_headers(
+            "short_service::request",
+            nats_headers,
+            retrieve_short_command.to_proto().encode_to_vec().into(),
+        )
+        .await
+    {
+        Ok(_) => info!(
+            "Sent {} with X-Correlation-Id '{}'",
+            retrieve_short_command.type_as_string(),
+            correlation_id.clone()
+        ),
+        Err(e) => info!(
+            "Failed to send message {} with X-Correlation-Id '{}' due to error: {}",
+            retrieve_short_command.type_as_string(),
+            correlation_id.clone(),
+            e
+        ),
+    }
+
+    match tokio::time::timeout(std::time::Duration::from_secs(10), rx).await {
+        Ok(Ok(response)) => {
+            debug!("Obtained response headers: {:?}", response.headers);
+            debug!("Obtained response message: {:?}", response.message);
+            let decoded_payload =
+                common::proto::messaging::v1::events::ShortRetrievedEvent::decode(
+                    response.message.payload,
+                )
+                .unwrap();
+            let retrieved_short_event = ShortRetrievedEvent::from(decoded_payload);
+            debug!("Retrieved short: {:?}", retrieved_short_event);
+            Redirect::temporary(&retrieved_short_event.short.long_url.as_str()).into_response()
         }
         _ => {
             warn!("Timed out waiting for response of '{}'", correlation_id);
