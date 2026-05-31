f905072efd1e8bb10be0a3c663ab717f56f50678
Michal Svačina <msvacina@outlook.com>
2026-05-31 13:27:40 +0200

    build: created db init script

--- db_init/db_init.sh
+++ db_init/db_init.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-DATABASE_URL=postgres://postgres:postgrespwd@localhost:5432/postgres
+#DATABASE_URL=postgres://postgres:postgrespwd@localhost:5432/postgres
+
+if [ -z "$DATABASE_URL" ]; then
+  echo "ERROR: No database URL set, please verify your settings"
+  exit 1
+fi
+
+DB_URL_CHECK=$(echo "$DATABASE_URL" | grep -Eo 'postgres://.*:.*@.*:([0-9]{0,})/.*' | wc -l)
+
+if [ "$DB_URL_CHECK" -eq 0 ]; then
+  echo "Connection string '$DATABASE_URL' doesn't seem to be valid, please check"
+  exit 2
+fi
+
+psql "$DATABASE_URL" <<EOF
+  SELECT 1;
+EOF
+
+if [ "$?" -ne 0 ]; then
+  echo "postgresql doesn't seem to be up, waiting 5 seconds"
+  sleep 5
+fi
 
 INIT_SQL="CREATE ROLE sas_app WITH
 LOGIN
@@ -36,3 +57,5 @@ echo "Initializing DB for Snack's Awesome Shortener"
 psql "$DATABASE_URL" << EOF
   $INIT_SQL
 EOF
+
+exit 0
